ecb1a604b4e82e302a40732a3335465e8ebd41cb
mrivandu <mrivandu@hotmail.com>
2019-03-25 14:49:09 +0800

    Update

--- DockerfilesAndYamls/Dockerfiles/set-ssd.sh
+++ DockerfilesAndYamls/Dockerfiles/set-ssd.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mkdir $SSD
-echo "$SSD *(fsid=0,rw,no_root_squash,no_subtree_check)">>exportfs
+echo "$SSD *(fsid=0,rw,no_root_squash,no_subtree_check)">>/etc/exports
 mount -t tmpfs -o size=100m  tmpfs $SSD
+exportfs -r
+rpcbind
+rpc.nfsd
+rpc.mountd
+rpc.rquotad
+rpc.statd
