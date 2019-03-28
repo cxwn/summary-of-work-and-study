7513be4e39668e20ed5a2ebdb1052dd174bd7025
耕耘实录 <mrivandu@hotmail.com>
2019-03-29 00:11:35 +0800

    Update

--- OpsScripts/SetLanguageAndCharacterset.sh
+++ OpsScripts/SetLanguageAndCharacterset.sh
@@ -3,12 +3,12 @@ LANGUAGE=zh_CN.UTF-8
 LANGPATH=/etc/locale.conf
 T_LANG=$(echo $LANG)
 echo "Current Environment:$LANG"
-locale #Show the current system langguage and character set 
+locale #Show the current system langguage and character set
 if [ "$LANGUAGE" !=  "$T_LANG" ]; then
-        echo "$LANGUAGE">$LANGPATH
-        echo "Congratulations!Done!"
-elif [ "$LANGUAGE" = "$T_LANG" ]; then
-        echo "Current system have been setup!"
+    echo "$LANGUAGE">$LANGPATH
+    echo "Congratulations!Done!"
+    elif [ "$LANGUAGE" = "$T_LANG" ]; then
+    echo "Current system have been setup!"
 else
-        echo "Sorry! System ERROR"
+    echo "Sorry! System ERROR"
 fi
