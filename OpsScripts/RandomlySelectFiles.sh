d179f3e63161e72795fc39acfd2ef3321e25d140
mrivandu <mrivandu@hotmail.com>
2019-04-22 12:32:52 +0800

    Update

--- OpsScripts/RandomlySelectFiles.sh
+++ OpsScripts/RandomlySelectFiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-src='/home/src'
-dst='/home/dst'
+# There are many files in a directory.We need to randomly select some specified files from these files.
+src='/home/src' #The path of source files.
+dst='/home/dst' #The path of destination files.
 counter=0
-needs=100
+needs=100 #The total number of requested files.
 for file in `ls ${src}/|sort -R`;do
 	mv ${src}/$(echo ${file}|awk -F "." '{print $1}').{TextGrid,wav} ${dst}/;
 	let counter=${counter}+1
