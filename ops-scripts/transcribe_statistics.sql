9e56b84d8165ddf45f76597bf0a1b5130f9f381c
mrivandu <mrivandu@hotmail.com>
2019-05-17 16:30:32 +0800

    update

--- ops-scripts/transcribe_statistics.sql
+++ ops-scripts/transcribe_statistics.sql
@@ -1,6 +1,6 @@
 select  sum(TranscribeAllSeconds) as '音频时长' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
 select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10';;
-select  sum(TranscribeFrequency) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= '2019-05-10';;
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay >= '2019-05-10';;
 select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10';;
 
 
@@ -9,3 +9,5 @@ select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (Pod
 
 select(select  sum(TranscribeFrequency) as '转写成功量' from transcribe_logs where PodType = 'AsyncService' and TranscribeDay >= '2019-05-10')
 /(select  sum(TranscribeFrequency) as '转写量'  from transcribe_logs where (PodType = 'AsyncServerError' or PodType = 'AsyncService' ) and TranscribeDay >= '2019-05-10') * 100 as '转写率';
+
+select  ifnull(sum(TranscribeFrequency),0) as '转写错误量'  from transcribe_logs where PodType = 'AsyncServerError' and TranscribeDay = '2019-05-10';
